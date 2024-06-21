5dde196d1a6a92cc884ac490f90c61ede63b772f
Hein Htet Aung <heinhtetaung405@gmail.coim>
2024-06-21 12:43:55 +0530

    1- auth controllers added

---
+++ backend/lib/util/generateToken.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+
+export const generateTokenAndSetCookie = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
+  res.cookie("jwt", token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000, //* Milliseconds
+    httpOnly: true, //! prevent xxs attacks and cross-site scripting attacks
+    sameSite: "strict", //! SCRF attacks cross-site request forgery attacks
+    secure: process.env.NODE_ENV !== "development",
+  });
+};
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import connectMongoDB from "./mongoDb/connet_Db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json()); //* To parse req.body
+app.use(express.urlencoded({ extended: true })) //* To parse form data (urlencoded)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
